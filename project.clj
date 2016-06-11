57bff5de6f87dbc1070017802e5ce6a18b1ad6d3
Greg Look <greg@greg-look.net>
2016-06-11 16:04:06 -0700

    Lein plugins shouldn't be namespaced.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/lein-monolith "0.1.0-SNAPSHOT"
+(defproject lein-monolith "0.1.0-SNAPSHOT"
   :description "Leiningen plugin for managing "
   :url "http://github.com/greglook/lein-monolith"
 
