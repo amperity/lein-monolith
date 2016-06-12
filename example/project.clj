1c131b09c6050fdccbb79c1fc17a70db6cc62d95
Greg Look <greg@greg-look.net>
2016-06-11 22:57:41 -0700

    Add example meta-project.

---
+++ example/project.clj
@@ -0,0 +1,9 @@
+(defproject example/all "0.0.0"
+  :description "Overarching example project."
+  :monolith true
+
+  :plugins
+  [[lein-monolith "0.1.0-SNAPSHOT"]]
+
+  :dependencies
+  [[org.clojure/clojure "1.8.0"]])
