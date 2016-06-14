dc3b34828edfb0c2cb8bfb59d0234c2526542796
Greg Look <greg@greg-look.net>
2016-06-14 16:06:24 -0700

    Merge dependencies in monolith profile.

--- src/lein_monolith/plugin.clj
+++ src/lein_monolith/plugin.clj
@@ -10,23 +10,55 @@
     [lein-monolith.config :as config]))
 
 
+(defn- dedupe-dependencies
+  "Given a vector of dependency coordinates, deduplicate and ensure there are no
+  conflicting versions found."
+  [dependencies]
+  (reduce-kv
+    (fn [current dep-name specs]
+      (let [specs (distinct specs)]
+        (if (= 1 (count specs))
+          ; Only one (unique) version declared, add to current vector.
+          (conj current (first specs))
+          ; Multiple versions or specs declared! Try to resolve.
+          (let [versions (distinct (map second specs))
+                projects (map (comp :monolith/project meta) specs)]
+            (if (= 1 (count versions))
+              ; Only one version in use, so the distinction must be something
+              ; like an :exclude directive in the spec. Use the first one and
+              ; warn about the conflict.
+              (let [choice (first specs)]
+                (lein/warn "Multiple dependency specs found for" dep-name
+                           "in projects" projects "- using" (pr-str choice)
+                           "from" (:monolith/project (meta choice)))
+                (conj current choice))
+              ; Multiple versions found, set the error flag and continue.
+              (lein/abort "Multiple dependency versions found for" dep-name
+                          "in projects" projects ":" versions))))))
+    []
+    (group-by first dependencies)))
+
+
 (defn monolith-profile
   "Constructs a profile map containing merged source and test paths."
   [config]
-  (let [projects (:internal-projects config)]
+  (->
     (reduce-kv
       (fn [profile project-name {:keys [dir]}]
         (lein/debug "Reading" project-name "subproject definiton from" dir)
-        (let [project (project/read (str (jio/file dir "project.clj")))]
+        (let [project (project/read (str (jio/file dir "project.clj")))
+              dependencies (map #(vary-meta % assoc :monolith/project project-name)
+                                (:dependencies project))]
           (-> profile
               (update :source-paths concat (:source-paths project))
-              (update :test-paths   concat (:test-paths   project))
-              ; TODO: merge :dependencies
-              )))
+              (update :test-paths   concat (:test-paths project))
+              (update :dependencies concat dependencies))))
       {:source-paths []
        :test-paths []
        :dependencies []}
-      projects)))
+      (:internal-projects config))
+    ; TODO: check dependency versions?
+    (update :dependencies dedupe-dependencies)))
 
 
 (defn add-profile
