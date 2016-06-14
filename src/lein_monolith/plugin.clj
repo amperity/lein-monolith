0849950671c20b447dd58bae66cca7e4ddba4c23
Greg Look <greg@greg-look.net>
2016-06-14 15:33:06 -0700

    Refactor lots of plugin commands.

--- src/lein_monolith/plugin.clj
+++ src/lein_monolith/plugin.clj
@@ -4,7 +4,9 @@
   value for the `:monolith` key."
   (:require
     [clojure.java.io :as jio]
-    [leiningen.core.project :as project]
+    (leiningen.core
+      [main :as lein]
+      [project :as project])
     [lein-monolith.config :as config]))
 
 
@@ -14,12 +16,12 @@
   (let [projects (:internal-projects config)]
     (reduce-kv
       (fn [profile project-name {:keys [dir]}]
+        (lein/debug "Reading" project-name "subproject definiton from" dir)
         (let [project (project/read (str (jio/file dir "project.clj")))]
           (-> profile
               (update :source-paths concat (:source-paths project))
               (update :test-paths   concat (:test-paths   project))
               ; TODO: merge :dependencies
-              ; TODO: set :target-path
               )))
       {:source-paths []
        :test-paths []
@@ -27,17 +29,33 @@
       projects)))
 
 
+(defn add-profile
+  "Adds the monolith profile to the given project if it's not already present."
+  [project]
+  (if (get-in project [:profiles :monolith/all])
+    project
+    (let [profile (monolith-profile (config/load!))]
+      (lein/debug "Adding monolith profile to project...")
+      (project/add-profiles project {:monolith/all profile}))))
+
+
+(defn activate-profile
+  "Activates the monolith profile in the project if it's not already active."
+  [project]
+  (if (contains? (set (:active-profiles (meta project))) :monolith/all)
+    project
+    (do (lein/debug "Merging monolith profile into project...")
+        (project/merge-profiles project [:monolith/all]))))
+
+
 (defn middleware
+  "Automatically adds the merged monolith profile to the project if it contains
+  a truthy value in `:monolith`."
   [project]
   (if (:monolith project)
     ; Monolith project, load up merged profile.
-    (let [config (config/load!)
-          profile (monolith-profile config)]
-      (println "Merging monolith profile into project")
-      #_
-      (-> project
-          (project/add-profiles {:monolith/all profile})
-          (project/merge-profiles [:monolith/all]))
-      (project/merge-profiles project [profile]))
+    (-> project
+        (add-profile)
+        (activate-profile))
     ; Normal project, don't activate.
     project))
--- src/leiningen/monolith.clj
+++ src/leiningen/monolith.clj
@@ -1,22 +1,21 @@
 (ns leiningen.monolith
+  "Leiningen task implementations for working with monorepos."
   (:require
     [clojure.java.io :as jio]
     [clojure.pprint :refer [pprint]]
     (leiningen.core
       [main :as lein]
       [project :as project])
-    [leiningen.with-profile :refer [with-profile]])
+    (lein-monolith
+      [config :as config]
+      [plugin :as plugin]))
   (:import
     (java.io
-      File
-      PushbackReader)
+      File)
     (java.nio.file
       Files
       LinkOption
       Paths)))
-
-
-(def config-name "monolith.clj")
 
 
 (defn- collapse-project-name
@@ -28,126 +27,64 @@
     sym))
 
 
-(defn- read-clj
-  "Read the first data structure in a clojure file."
-  [file]
-  (-> (jio/file file)
-      (jio/reader)
-      (PushbackReader.)
-      (read)))
 
+;; ## Subtask Implementations
 
-(defn- read-project-coord
-  "Reads a leiningen project definition from the given directory and returns a
-  vector of the project's name symbol and version. Returns nil if the project
-  file does not exist or is invalid."
-  [dir]
-  (let [project-file (jio/file dir "project.clj")]
-    (when-let [project (and (.exists project-file) (read-clj project-file))]
-      (if (and (list? project) (= 'defproject (first project)))
-        [(nth project 1) (nth project 2)]
-        (println "WARN:" (str project-file) "does not appear to be a valid leiningen project definition!")))))
-
-
-(defn- find-internal-projects
-  "Returns a sequence of vectors containing the project name and the path to
-  the project's directory."
-  [root project-dirs]
-  (when root
-    (->>
-      project-dirs
-      (mapcat
-        (fn list-projects
-          [path]
-          (let [projects-dir (jio/file root path)]
-            (->> (.listFiles projects-dir)
-                 (map #(vector (read-project-coord %) %))
-                 (filter first))))))))
-
-
-(defn- find-config
-  "Searches upward from the project root until it finds a configuration file.
-  Returns the `File` object if found, or nil if no matching file could be
-  located in the parent directories."
-  ^java.io.File
-  [dir]
-  (when dir
-    (let [dir (jio/file dir)
-          file (jio/file dir config-name)]
-      (if (.exists file)
-        file
-        (recur (.getParent dir))))))
-
-
-(defn- load-config!
-  "Reads the monolith configuration file and returns the contained data
-  structure."
-  ([]
-   (load-config! (System/getProperty "user.dir")))
-  ([dir]
-   (let [file (find-config dir)]
-     (when-not file
-       (lein/abort "Could not find configuration file" config-name "in any parent directory of" dir))
-     (let [root (.getParent file)
-           config (read-clj file)
-           projects (->> (find-internal-projects root (:project-dirs config))
-                         (map (fn [[[pname version] dir]]
-                                [pname {:version version, :dir dir}]))
-                         (into {}))]
-       (assoc config
-              :config-path (str file)
-              :mono-root (str root)
-              :internal-projects projects)))))
-
-
-(defn- internal-project?
-  "Determines whether the given project symbol names a project defined inside
-  the monorepo."
-  [config project]
-  (boolean (get-in config [:internal-projects project])))
-
-
-(defn- merged-profile
-  "Constructs a profile map containing merged source and test paths."
-  [config]
-  (let [projects (:internal-projects config)]
-    (reduce-kv
-      (fn [profile dependency {:keys [dir]}]
-        (let [project (project/read (str (jio/file dir "project.clj")))]
-          (-> profile
-              (update :source-paths concat (:source-paths project))
-              (update :test-paths   concat (:test-paths   project))
-              ; TODO: dependencies?
-              )))
-      {:source-paths []
-       :test-paths []
-       :dependencies []}
-      projects)))
-
-
-
-;; ## Command Implementations
-
-(defn- print-info
-  "Prints some information about the monorepo configuration."
+(defn ^:no-project-needed info
+  "Show information about the monorepo configuration."
   []
-  (let [config (load-config!)]
+  (let [config (config/load!)]
     (println "Config path:" (:config-path config))
     (println)
     (println "Internal projects:")
+    ; TODO: puget?
     (let [prefix-len (inc (count (:mono-root config)))]
       (doseq [[pname {:keys [version dir]}] (:internal-projects config)]
-        (printf "  %-40s -> %s\n" (pr-str [pname version]) (subs (str dir) prefix-len))))))
+        (printf "  %-40s   %s\n" (pr-str [pname version]) (subs (str dir) prefix-len))))))
 
 
-(defn- link-checkouts!
-  "Create symlinks in the checkouts directory pointing to all internal
-  dependencies in the current project."
+(defn check-deps
+  "Check the versions of external dependencies of the current project.
+
+  Options:
+    :unlocked    Print warnings for external dependencies with no specified version
+    :strict      Exit with a failure status if any versions don't match"
   [project args]
-  (let [config (load-config!)
+  (let [config (config/load!)
+        options (set args)
+        ext-deps (->> (:external-dependencies config)
+                      (map (juxt first identity))
+                      (into {}))
+        error-flag (atom false)]
+    (doseq [[pname :as spec] (:dependencies project)]
+      (let [pname' (collapse-project-name pname)
+            spec' (vec (cons pname' (rest spec)))]
+        (when-not (config/internal-project? config pname')
+          (if-let [expected-spec (ext-deps pname')]
+            (when-not (= expected-spec spec')
+              (lein/warn "ERROR: External dependency" (pr-str spec') "does not match expected spec" (pr-str expected-spec))
+              (when (get options ":strict")
+                (reset! error-flag true)))
+            (when (get options ":unlocked")
+              (lein/warn "WARN: External dependency" (pr-str pname') "has no expected version defined"))))))
+    (when @error-flag
+      (lein/abort))))
+
+
+(defn link
+  "Create symlinks in the checkouts directory pointing to all internal
+  dependencies in the current project.
+
+  Options:
+    :force       Override any existing checkout links with conflicting names"
+  [project args]
+  (when (:monolith project)
+    (lein/abort "The 'link' task does not need to be run for the monolith project!"))
+  (let [config (config/load!)
         options (set args)
         checkouts-dir (jio/file (:root project) "checkouts")]
     (when-not (.exists checkouts-dir)
+      (lein/debug "Creating checkout directory" checkouts-dir)
       (.mkdir checkouts-dir))
     (doseq [spec (:dependencies project)
             :let [dependency (collapse-project-name (first spec))]]
@@ -160,93 +97,55 @@
             (let [actual-target (Files/readSymbolicLink link)]
               (if (and (Files/isSymbolicLink link) (= target actual-target))
                 ; Link exists and points to target already.
-                (println "Link for" dependency "is correct")
+                (lein/info "Link for" dependency "is correct")
                 ; Link exists but points somewhere else.
                 (if (get options "--force")
                   ; Recreate link since :force is set.
-                  (do (println "Relinking" dependency "from"
-                               (str actual-target) "to" (str target))
+                  (do (lein/warn "Relinking" dependency "from"
+                                 (str actual-target) "to" (str target))
                       (Files/delete link)
                       (create-link!))
                   ; Otherwise print a warning.
-                  (println "WARN:" dependency "links to" (str actual-target)
-                           "instead of" (str target)))))
+                  (lein/warn "WARN:" dependency "links to" (str actual-target)
+                             "instead of" (str target)))))
             ; Link does not exist, so create it.
-            (do (println "Linking" dependency "to" (str target))
+            (do (lein/info "Linking" dependency "to" (str target))
                 (create-link!))))))))
 
 
-(defn- check-dependencies
-  "Check the versions of external dependencies of the current project."
-  [project args]
-  (let [config (load-config!)
-        options (set args)
-        ext-deps (->> (:external-dependencies config)
-                      (map (juxt first identity))
-                      (into {}))
-        error-flag (atom false)]
-    (doseq [[pname :as spec] (:dependencies project)]
-      (let [pname' (collapse-project-name pname)
-            spec' (vec (cons pname' (rest spec)))]
-        (when-not (internal-project? config pname')
-          (if-let [expected-spec (ext-deps pname')]
-            (when-not (= expected-spec spec')
-              (lein/warn "ERROR: External dependency" (pr-str spec') "does not match expected spec" (pr-str expected-spec))
-              (when (get options "--strict")
-                (reset! error-flag true)))
-            (when (get options "--unlocked")
-              (lein/warn "WARN: External dependency" (pr-str pname') "has no expected version defined"))))))
-    (when @error-flag
-      (lein/abort))))
+(defn ^:higher-order with-all
+  "Apply the given task with a merged set of dependencies, sources, and tests
+  from all the internal projects.
 
+  For example:
 
-(defn- apply-with-all
-  "Execute the following commands in the context of a merged set of source and
-  test paths."
-  [project args]
-  (let [config (load-config!)
-        profile (merged-profile config)]
-    (apply with-profile
-      (project/add-profiles project {:monolith/all profile})
-      "+monolith/all"
-      args)))
+      lein monolith with-all test"
+  [project task-name & args]
+  (when (:monolith project)
+    (lein/abort "Running 'with-all' in a monolith project is redundant!"))
+  (let [config (config/load!)
+        profile (plugin/monolith-profile config)]
+      ; Figure out list of active profiles and the merged profile.
+      (lein/apply-task
+        task-name
+        (-> project
+            (assoc-in [:profiles :monolith/all] profile)
+            (project/set-profiles (conj (:active-profiles (meta project)) :monolith/all)))
+        args)))
 
 
 
 ;; ## Plugin Entry
 
-(defn ^:no-project-needed monolith
-  "Entry point for the monolith task. Second arg should be a command, followed
-  by any command-specific arguments."
-  [project & [command & args]]
+(defn monolith
+  "Tasks for working with Leiningen projects inside a monorepo."
+  {:subtasks [#'info #'link #'check-deps #'with-all]}
+  [project command & args]
   (case command
-    (nil "help")
-      (do
-        (println "Usage: lein monolith <command> [args...]")
-        (println)
-        (println "    info         Print some information about the current configuration")
-        (println "    checkout     Set up checkout dependency links to internal projects")
-        (println "      --force       Override existing checkout links")
-        (println "    deps         Check external dependency versions against the approved list")
-        (println "      --unlocked    Print warnings for external dependencies with no specified version")
-        (println "      --strict      Exit with a failure status if any versions don't match")
-        (println "    with-all     Run the following commands with a merged profile of all project sources")
-        (println "    help         Show this help message"))
-
-    "debug"
-      (pprint (load-config!))
-
-    "info"
-      (print-info)
-
-    "checkout"
-      (link-checkouts! project args)
-
-    "deps"
-      (check-dependencies project args)
-
-    "with-all"
-      (apply-with-all project args)
-
+    "debug"      (pprint (config/load!))
+    "info"       (info)
+    "link"       (link project args)
+    "check-deps" (check-deps project args)
+    "with-all"   (apply with-all project args)
     (lein/abort (pr-str command) "is not a valid monolith command! (try \"help\")"))
   (flush))
