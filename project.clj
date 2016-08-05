b81e1cf3882e997630be60c6d7a1c30e106cbcff
Greg Look <greg@greg-look.net>
2016-08-05 11:53:34 -0700

    Bump snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-monolith "0.2.1"
+(defproject lein-monolith "0.2.2-SNAPSHOT"
   :description "Leiningen plugin for managing subrojects within a monorepo."
   :url "https://github.com/amperity/lein-monolith"
   :license {:name "Apache License 2.0"
