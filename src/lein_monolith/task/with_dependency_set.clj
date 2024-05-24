75ab550da555cc59690b6124daf965d0c051cae0
Stephen Gerkin <stephen.gerkin@amperity.com>
2024-05-24 12:46:20 -0400

    use a profile to override deps

--- src/lein_monolith/task/with_dependency_set.clj
+++ src/lein_monolith/task/with_dependency_set.clj
@@ -1,39 +1,8 @@
 (ns lein-monolith.task.with-dependency-set
   (:require
-    [clojure.java.io :as io]
     [lein-monolith.plugin :as plugin]
     [lein-monolith.task.util :as u]
-    [leiningen.core.main :as lein]
-    [leiningen.core.project :as project]))
-
-
-(defn- reload-project
-  "Reloads the raw project.clj file for the given project.
-   This has the effect of removing any middleware that was added to the project
-   as well as any changes to the project map that were made in the current
-   session."
-  [project]
-  (let [project-file (io/file (:root project) "project.clj")]
-    (project/read-raw (str project-file))))
-
-
-(defn- change-dependencies
-  [project dependency-set]
-  (let [[monolith _] (u/load-monolith! project)
-        dependencies (or (get-in monolith [:monolith :dependency-sets dependency-set])
-                         (lein/abort (format "Unknown dependency set %s" dependency-set)))]
-    (if (:monolith project)
-      (assoc project :managed-dependencies dependencies)
-      (assoc project :monolith/dependency-set dependency-set))))
-
-
-(defn- setup
-  [project dependency-set]
-  (-> project
-      (reload-project)
-      (change-dependencies dependency-set)
-      (plugin/add-middleware)
-      (project/init-project)))
+    [leiningen.core.main :as lein]))
 
 
 (defn run-task
@@ -41,6 +10,12 @@
    the project, changing the dependencies, re-initializing the project, and then
    running the task."
   [project dependency-set task]
-  (-> project
-      (setup dependency-set)
-      (lein/resolve-and-apply task)))
+  (let [[monolith _] (u/load-monolith! project)
+        dependencies (or (get-in monolith [:monolith :dependency-sets dependency-set])
+                         (lein/abort (format "Unknown dependency set %s" dependency-set)))
+        managed-deps {:managed-dependencies (with-meta dependencies {:replace true})}
+        profile (merge managed-deps
+                       (when-not (:monolith project)
+                         {:monolith/dependency-set dependency-set}))
+        project (plugin/add-active-profile project :monolith/dependency-override profile)]
+    (lein/resolve-and-apply project task)))
--- test/lein_monolith/task/with_dependency_set_test.clj
+++ test/lein_monolith/task/with_dependency_set_test.clj
@@ -1,7 +1,6 @@
 (ns lein-monolith.task.with-dependency-set-test
   (:require
     [clojure.test :refer [deftest testing is]]
-    [lein-monolith.plugin :as plugin]
     [lein-monolith.task.with-dependency-set :as wds]
     [lein-monolith.test-utils :refer [use-example-project]]
     [leiningen.core.main :as lein]
@@ -11,39 +10,30 @@
 (use-example-project)
 
 
-(defn- remove-unstable
-  [project]
-  (assoc-in project [:monolith :project-selectors :unstable] "non-deterministic"))
-
-
 (defn- read-project
   [path]
-  (-> (project/read-raw path)
-      (plugin/add-middleware)
-      (project/init-project)))
+  (project/read path))
 
 
 (deftest setup-test
   (with-redefs [lein/resolve-and-apply (fn [project & _] project)]
-    (let [project (remove-unstable (read-project "example/project.clj"))]
-      (testing "Root Project"
-        (is (= (assoc project
-                      :managed-dependencies
-                      '([amperity/greenlight "0.7.1"]
-                        [org.clojure/spec.alpha "0.3.218"]))
-               (remove-unstable (wds/run-task project :set-a nil)))
-            "For root project, without :only, should have managed dependencies from :set-a")))
-    (let [project (read-project "example/apps/app-a/project.clj")]
-      (testing "Subproject"
-        (let [expected (-> project
-                           (assoc :monolith/dependency-set :set-outdated
-                                  :managed-dependencies '([amperity/greenlight "0.7.0"]
-                                                          [org.clojure/spec.alpha "0.2.194"]
-                                                          [amperity/greenlight "0.6.0"]
-                                                          [com.amperity/vault-clj "2.1.583"]))
-                           (assoc-in [:profiles :monolith/dependency-set :managed-dependencies]
-                                     '[[amperity/greenlight "0.7.0"]
-                                       [org.clojure/spec.alpha "0.2.194"]]))]
-          (is (= expected
-                 (wds/run-task project :set-outdated nil))
-              "For subproject, should have managed dependencies from :set-outdated"))))))
+    (testing "Root Project"
+      (let [project (read-project "example/project.clj")
+            deps '([amperity/greenlight "0.7.1"]
+                   [org.clojure/spec.alpha "0.3.218"])
+            actual (wds/run-task project :set-a nil)]
+        (is (= deps (:managed-dependencies actual)))
+        (is (= deps
+               (get-in actual [:profiles :monolith/dependency-override :managed-dependencies])))))
+    (testing "Subproject"
+      (let [project (read-project "example/apps/app-a/project.clj")
+            dep-set-deps '[[amperity/greenlight "0.7.1"]
+                           [org.clojure/spec.alpha "0.3.218"]]
+            replaced-deps '[[amperity/greenlight "0.7.0"]
+                            [org.clojure/spec.alpha "0.2.194"]]
+            actual (wds/run-task project :set-outdated nil)]
+        (is (= replaced-deps (:managed-dependencies actual)))
+        (is (= replaced-deps
+               (get-in actual [:profiles :monolith/dependency-override :managed-dependencies])))
+        (is (= :set-outdated (:monolith/dependency-set actual)))
+        (is (= dep-set-deps (get-in actual [:profiles :monolith/dependency-set :managed-dependencies])))))))
