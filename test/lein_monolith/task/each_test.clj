3c2ed2d3c14afbfe8b46bf9bfadf3aee85457552
Daniel Cardenas <daniel.cardenas@amperity.com>
2024-10-15 13:08:34 -0500

    PR feedback

--- test/lein_monolith/task/each_test.clj
+++ test/lein_monolith/task/each_test.clj
@@ -20,19 +20,19 @@
   (testing "Verify that the clean targets for each subproject are cleaned up by `lein monolith each clean`."
     (let [monolith (read-example-project)
           subprojects (config/read-subprojects! monolith)]
-      (doseq [[_subproject-name subproject] subprojects]
-        (doseq [target (:clean-targets subproject)]
-          (let [path (test-path subproject target)]
-            (is (str/starts-with? path (:root subproject))
-                "The test file path should be created within the subproject directory.")
-            (io/make-parents path)
-            (spit path "test")
-            (is (.exists (io/file path)) "The test file should have been created."))))
+      (doseq [[_subproject-name subproject] subprojects
+              target (:clean-targets subproject)]
+        (let [path (test-path subproject target)]
+          (is (str/starts-with? path (:root subproject))
+              "The test file path should be created within the subproject directory.")
+          (io/make-parents path)
+          (spit path "test")
+          (is (.exists (io/file path)) "The test file should have been created.")))
       (each/run-tasks monolith {} ["clean"]) ; lein monolith each clean
-      (doseq [[_subproject-name subproject] subprojects]
-        (doseq [target (:clean-targets subproject)]
-          (let [path (test-path subproject target)
-                test-file (io/file path)
-                parent-dir (io/file (.getParent test-file))]
-            (is (not (.exists test-file)) "The test file should not exist after a lein clean")
-            (is (not (.exists parent-dir)) "The target directory should not exist after a lein clean")))))))
+      (doseq [[_subproject-name subproject] subprojects
+              target (:clean-targets subproject)]
+        (let [path (test-path subproject target)
+              test-file (io/file path)
+              parent-dir (io/file (.getParent test-file))]
+          (is (not (.exists test-file)) "The test file should not exist after a lein clean")
+          (is (not (.exists parent-dir)) "The target directory should not exist after a lein clean"))))))
